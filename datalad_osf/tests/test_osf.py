3f27fb78003298723fba95ed9acafd458726a806
mjboos <moritz.boos@uni-oldenburg.de>
2020-06-17 18:11:38 +0200

    removed mock test for osf

--- datalad_osf/tests/test_osf.py
+++
@@ -1,6 +0,0 @@
-# mock test to see if travis correctly decrypts
-def test_travis_decrypt():
-    import os
-    assert 'OSF_USER' in os.environ
-    assert 'OSF_PASSWORD' in os.environ
-    assert 'OSF_TOKEN' in os.environ

61593daf682c61d74b10c5cd681528d1d9bf4457
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2020-06-17 13:06:25 +0200

    add basic helpers

---
+++ datalad_osf/utils.py
@@ -0,0 +1,80 @@
+from osfclient import OSF
+from os import environ
+import json
+
+osf = OSF(username=environ['OSF_USERNAME'],
+          password=environ['OSF_PASSWORD'])
+
+
+# Note: This should ultimately go into osfclient
+def create_project(title, category="project", tags=None):
+    """ Create a project on OSF
+
+    Parameters
+    ----------
+    title: str
+        Title of the project
+    category: str
+        categorization changes how the project is displayed
+        on OSF, but doesn't appear to have a "real" function
+    tags: list of str
+
+    Returns
+    -------
+    str
+        ID of the created project
+    """
+
+    url = osf.session.build_url('nodes')
+    post_data = {"data":
+                     {"type": "nodes",
+                      "attributes":
+                          {"title": title,
+                           "category": category
+                           }
+                      }
+                 }
+    if tags:
+        post_data["data"]["attributes"]["tags"] = tags
+
+    response = osf.session.post(url, data=json.dumps(post_data))
+    # TODO: figure what errors to better deal with /
+    #       create a better message from
+    response.raise_for_status()
+
+    # TODO: This should eventually return an `Project` instance (see osfclient).
+    #       Response contains all properties of the created project.
+    return response.json()['data']['id']
+
+
+def initialize_osf_remote(remote, project, objpath="git-annex",
+                          encryption="none", autoenable="true"):
+    """Initialize special remote with a given project
+
+    convenience wrapper for git-annex-initremote w/o datalad
+
+    Parameters
+    ----------
+    remote: str
+        name for the special remote
+    project: str
+        ID of the project/component to use
+    objpath: str
+        path in the project's storage to be used for
+        annex objects
+    encryption: str
+        see git-annex-initremote; mandatory option;
+    autoenable: str
+        'true' or 'false'; tells git-annex to automatically enable the
+        special remote on git-annex-init (particularly after a fresh git-clone
+    """
+
+    init_opts = ["type=external",
+                 "externaltype=osf",
+                 "encryption={}".format(encryption),
+                 "autoenable={}".format(autoenable),
+                 "project={}".format(project),
+                 "objpath={}".format(objpath)]
+
+    import subprocess
+    subprocess.run(["git", "annex", "initremote", remote] + init_opts)

a4553fdd8eaa8a8cc11b202ac91264238d4002cf
Michael Hanke <michael.hanke@gmail.com>
2020-06-18 08:06:34 +0200

    Enabled test to pull credentials from a credential store too
    Otherwise no change: envvar takes preference. Both token (preferred)
    and user/pass fallback are supported.

--- datalad_osf/tests/utils.py
+++ datalad_osf/tests/utils.py
@@ -7,15 +7,44 @@ from datalad_osf.utils import (
     create_project,
     delete_project,
 )
+from datalad.downloaders.credentials import (
+    Token,
+    UserPassword,
+)
 from datalad_osf.osfclient.osfclient import OSF
 
 
 @optional_args
 def with_project(f, osf_session=None, title=None, category="project"):
+    token_auth = Token(name='https://osf.io', url=None)
+    up_auth = UserPassword(name='https://osf.io', url=None)
 
-    # TODO: token auth!
-    osf = OSF(username=environ['OSF_USERNAME'],
-              password=environ['OSF_PASSWORD'])
+    # get auth token, form environment, or from datalad credential store
+    # if known-- we do not support first-time entry during a test run
+    token = environ.get(
+        'OSF_TOKEN',
+        token_auth().get('token', None) if token_auth.is_known else None)
+    username = None
+    password = None
+    if not token:
+        # now same for user/password if there was no token
+        username = environ.get(
+            'OSF_USERNAME',
+            up_auth().get('user', None) if up_auth.is_known else None)
+        password = environ.get(
+            'OSF_PASSWORD',
+            up_auth().get('password', None) if up_auth.is_known else None)
+
+    # place into environment, for now this is the only way the special remote
+    # can be supplied with credentials
+    for k, v in (('OSF_TOKEN', token),
+                 ('OSF_USERNAME', username),
+                 ('OSF_PASSWORD', password)):
+        if v:
+            environ[k] = v
+    # supply both auth credentials, so osfclient can fall back on user/pass
+    # if needed
+    osf = OSF(username=username, password=password, token=token)
 
     @wraps(f)
     def new_func(*args, **kwargs):
